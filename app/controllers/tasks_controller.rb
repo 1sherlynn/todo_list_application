e80339ee6e75b12d2aefb8be0e61e3836762c935
Sherlynn Tan <1sherlynn@gmail.com>
2017-09-05 00:47:21 +0800

    added Edit, Update and Destroy actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,22 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update 
+		if @task.update(task_params)
+			redirect_to task_path(@task) 
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy 
+		@task.destroy
+		redirect_to root_path
+	end
+
 	private
 	def task_params 
 		params.require(:task).permit(:title, :description)
@@ -30,6 +46,5 @@ class TasksController < ApplicationController
 	def find_task
 		@task = Task.find(params[:id])
 	end
-
 
 end
